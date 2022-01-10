266f5b3e9d77d4573a06fe72cef2a293bb7fe842
harihaysrun <nursyahirah98@outlook.com>
2022-01-10 07:21:44 +0000

    trying to add the Create function

--- index.js
+++ index.js
@@ -15,14 +15,31 @@ const {ReadPreferenceMode} = require('mongodb');
 const MONGO_URI = process.env.MONGO_URI;
 // console.log("MONGO_URI: " + MONGO_URI);
 
+async function getDBname(){
+    await MongoUtil.connect(MONGO_URI,  "skincare_api");
+    // console.log("getting skincare_api database")
+}
+
+getDBname();
+
 app.get('/', function(req,res){
     // res.send('hello there')
     res.render('index');
 })
 
-app.get('/skincare-products', function(req,res){
+app.get('/skincare-products', async function(req,res){
+    const db = MongoUtil.getDB();
+    let skincareProducts = await db.collection('skincare_products');
+
+    res.render('skincare-products', {
+        'skincareProducts': skincareProducts
+    });
     res.render('skincare-products');
 })
+
+app.get('/skincare-products/add'), function(req,res){
+    res.render('add-skincare-product');
+}
 
 app.listen(3000, function(){
     console.log("server has started")
