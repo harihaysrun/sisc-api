4f0869081a92d2e460bf65921c554db0b6cb0c93
harihaysrun <nursyahirah98@outlook.com>
2022-01-28 14:03:28 +0000

    search up to skinType

--- index.js
+++ index.js
@@ -181,18 +181,20 @@ app.post('/search', async function(req,res){
                     '$regex': req.body.search, '$options': 'i'
                     }
                 },
-                // {'productName': {
-                //     '$regex': req.body.search, '$options':'i'
-                //     }
-                // },
-                {'productCondition': req.body.productCondition}
-                // {'productCategory': req.body.productCategory},
-                // {'productType': req.body.productType},
-                // {'productPriceDollars': {
-                //     '$lte': req.body.productPriceDollars
-                //     }
-                // },
-                // {'skinType': req.body.skinType},
+                {'productName': {
+                    '$regex': req.body.search, '$options':'i'
+                    }
+                },
+                {'productCondition': req.body.productCondition},
+                {'productCategory': req.body.productCategory},
+                {'productType': req.body.productType},
+                {'productPriceDollars': {
+                    '$lte': req.body.productPriceDollars
+                    }
+                },
+                {'skinType': { 
+                    '$in': [req.body.skinType]
+                }},
                 // {'productVegan': req.body.productVegan},
                 // {'productCrueltyFree': req.body.productCrueltyFree},
                 ]
