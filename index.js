67cf9879202c0a2a1f7a8aa515d76b5a148b7c4f
harihaysrun <nursyahirah98@outlook.com>
2022-02-01 12:08:03 +0000

    removed name from edit pages

--- index.js
+++ index.js
@@ -134,7 +134,7 @@ app.patch('/skincare-products/:id', async function(req,res){
         '_id': new ObjectId(id),
     },{
         '$set':{
-            'posterName': req.body.posterName,
+            // 'posterName': req.body.posterName,
             'productCondition': req.body.productCondition,
             'productBrand': req.body.productBrand,
             'productName': req.body.productName,
@@ -268,7 +268,7 @@ app.patch('/requested-products/:id', async function(req,res){
         '_id': new ObjectId(id),
     },{
         '$set':{
-            'posterName': req.body.posterName,
+            // 'posterName': req.body.posterName,
             'productCondition': req.body.productCondition,
             'productBrand': req.body.productBrand,
             'productName': req.body.productName,
