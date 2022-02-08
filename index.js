cf6e295f98baa9dfdb614dda18f5c00825292548
harihaysrun <nursyahirah98@outlook.com>
2022-02-08 04:28:29 +0000

    Add date to request

--- index.js
+++ index.js
@@ -288,7 +288,7 @@ app.patch('/requested-products/:id', async function(req,res){
         '_id': new ObjectId(id),
     },{
         '$set':{
-            // 'posterName': req.body.posterName,
+            'datePosted': req.body.datePosted,
             'productCondition': req.body.productCondition,
             'productBrand': req.body.productBrand,
             'productName': req.body.productName,
